90c6943a2cf0654e4014dd3a4d3ea5381a4eae71
Padchara Teeduang <jab-love@hotmail.co.th>
2018-12-15 10:14:19 +0700

    delete url

--- database.js
+++ database.js
@@ -1,8 +1,8 @@
 var pgp = require('pg-promise')();
-var access = process.env.getDb + "?ssl=true";
+var access = process.env.getDb;
 console.log(access);
-// var db = pgp("postgres://mqvvplpgwkwaww:bbe283a5d4ce86d696c0794bfca51f66630f8553225b83bff3880b169880250a@ec2-54-243-147-162.compute-1.amazonaws.com:5432/ddhq6akm6qbbtp?ssl=true");
 var db=pgp(access)
+
 function getAllProducts(req, res) {
     db.any('select * from products')
         .then(function (data) {
